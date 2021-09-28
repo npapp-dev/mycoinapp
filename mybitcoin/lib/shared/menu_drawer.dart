aee5d1f2cf9881e47bc547e30ec0464d070a39f2
npapp-dev <Orient91.PN>
2021-09-27 21:18:18 -0400

    Git reinit

---
+++ mybitcoin/lib/shared/menu_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: buildMenuItems(context),
+      )
+    );
+  }
+
+  List<Widget> buildMenuItems(BuildContext context){
+    final List<String> menuTitles =[
+      'Home',
+      'Exchange rate',
+    ];
+    List<Widget> menuItems = [];
+    menuItems.add(DrawerHeader(
+      decoration: BoxDecoration(color: Colors.blueGrey),
+      child: Text('Coin app',
+        style: TextStyle(color: Colors.white, fontSize: 28))
+    ));
+    menuTitles.forEach((String element){
+        menuItems.add(ListTile(
+          title: Text(element,
+          style: TextStyle(fontSize: 18)),
+          onTap: (){}
+        ));
+    });
+    return menuItems;
+  }
+}
